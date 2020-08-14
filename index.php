b47b2a9e7390d660ebadd9fb26475ac4c98733e1
AdenSTUDIO <69081632+AdenSTUDIO@users.noreply.github.com>
2020-08-14 21:41:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$DB_ServerName = 'sql12.freesqldatabase.com';
+$DB_ServerName = 'sql2.freesqldatabase.com';
 $DB_UserName = 'sql12360350';
 $DB_Password = 'CjscyaTLcu';
 $DB_Name = 'sql12360350';
@@ -9,7 +9,7 @@ $conn = new mysqli($DB_ServerName, $DB_UserName, $DB_Password, $DB_Name);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  exit;
 }
 echo "Connected successfully";
 
