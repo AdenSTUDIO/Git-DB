d2b0106ce4d1f0b12cac012e031fba4ea7b511c3
AdenSTUDIO <69081632+AdenSTUDIO@users.noreply.github.com>
2020-08-14 21:51:20 +0700

    GITHUB Commit

---
+++ PHPtoDB.php
@@ -0,0 +1,15 @@
+<?php
+
+$DB_ServerName = 'sql12.freesqldatabase.com';
+$DB_UserName = 'sql12360350';
+$DB_Password = 'CjscyaTLcu';
+$DB_Name = 'sql12360350';
+
+$conn = new mysqli($DB_ServerName, $DB_UserName, $DB_Password, $DB_Name);
+
+// Check connection
+if ($conn->connect_error) {
+  exit;
+}
+
+?>
